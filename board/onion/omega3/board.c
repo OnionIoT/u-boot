d22fb20c4f7b51bdf47c883a041e8ac84be04ea4
Lazar <lazardemin@gmail.com>
2024-08-23 10:01:17 -0400

    no longer setting gpio11 to high; added descriptive comments to other register operations

--- board/onion/omega3/board.c
+++ board/onion/omega3/board.c
@@ -26,33 +26,28 @@ void gpio_init(void)
     u32 val;
     printf( "Initializing MT7688 GPIO system.\n" );
     
-    //set gpio2_mode 1:0=2b01 wled,p0,p1,p2,p3,p4 as gpio
+    //set gpio2_mode - setting wled, and p0,p1,p2,p3,p4 LED pins to GPIO mode
     val = 0x555;
     OMEGA3_REG(OMEGA3_SYS_CNTL_BASE+0x64)=val; // GPIO2_MODE register
-    OMEGA3_REG(0xb0000644)=0x0f<<7;
+    OMEGA3_REG(0xb0000644)=0x0f<<7; // GINT_FEDGE_1: setting "Enable falling edge triggered" for GPIOs 39-42
     
+    // set gpio_ctrl_1 register: set GPIO44 to output
     //gpio44 output gpio_ctrl_1 bit3=1
     val=OMEGA3_REG(OMEGA3_REG_PIODIR+0x04);
     val|=1<<12;
     OMEGA3_REG(OMEGA3_REG_PIODIR+0x04)=val;
     
+    // set gpio1_mode register: set WDT_MODE to GPIO mode
     //set gpio1_mode 14=1b1
     val=OMEGA3_REG(OMEGA3_SYS_CNTL_BASE+0x60);
     val|=1<<14;
     OMEGA3_REG(OMEGA3_SYS_CNTL_BASE+0x60)=val;
     
+    // set gpio_ctrl_1 resgister: set GPIO38 to input
     //gpio38 input gpio_ctrl_1 bit5=0
     val=OMEGA3_REG(OMEGA3_REG_PIODIR+0x04);
     val&=~1<<6;
     OMEGA3_REG(OMEGA3_REG_PIODIR+0x04)=val;
-
-    //setting GPIO 11 High, required for the reset button to work
-    val=OMEGA3_REG(OMEGA3_REG_PIODIR);
-    val|=1<<11;
-    OMEGA3_REG(OMEGA3_REG_PIODIR) = val; // GPIO 11 direction output
-    val=OMEGA3_REG(OMEGA3_REG_PIODATA);
-    val|=1<<11;
-    OMEGA3_REG(OMEGA3_REG_PIODATA) = val; // GPIO 11 High
 }
 
 int  board_late_init (void)
