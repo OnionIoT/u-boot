b718b09a9389d3375bf9b084983bb5121d1c9224
Philip Oberfichtner <pro@denx.de>
2025-04-23 08:08:30 +0200

    mips: mt7628: Add Onion Omega2/2+ board support
    The Omega2 / Omega2+ are based on the MediaTek MT7688 SoC with the
    following features:
        - 64 / 128MB of DDR2 memory
        - 16 / 32MB of SPI NOR flash
        - USB, WiFi and many more peripherals
    
    Signed-off-by: Philip Oberfichtner <pro@denx.de>

---
+++ include/configs/onion-omega2p.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2024 Zheng Han <zh@onioniot.com>, Lazar Demin <lazar@onioniot.com>
+ */
+#ifndef __CONFIG_ONION_OMEGA2P_H
+#define __CONFIG_ONION_OMEGA2P_H
+
+/* RAM */
+#define CFG_SYS_SDRAM_BASE      0x80000000
+#define CFG_SYS_INIT_SP_OFFSET  0x400000
+
+/* Dummy value */
+#define CFG_SYS_UBOOT_BASE      0
+
+#endif /* __CONFIG_ONION_OMEGA2P_H */
