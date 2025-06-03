fe20fa331cd03df579beca42efb57bc32bc35e22
Philip Oberfichtner <pro@denx.de>
2025-06-03 12:04:37 +0200

    mips: serial: Silence "unused variable" warning
    Before this commit, mtmips_spl_serial_init() caused the following
    warning:
    
      arch/mips/mach-mtmips/mt7628/serial.c:14:23: warning: unused variable ‘base’ [-Wunused-variable]
         14 |         void __iomem *base = ioremap_nocache(SYSCTL_BASE, SYSCTL_SIZE);
            |                       ^~~~
    
    This happens whenever CONFIG_SPECIFY_CONSOLE_INDEX is unset, which leads
    to an effectively empty function:
    
      void mtmips_spl_serial_init(void)
      {
    	  void __iomem *base = ioremap_nocache(SYSCTL_BASE, SYSCTL_SIZE);
      }
    
    Add the __maybe_unused attribute to silence this warning.
    
    Signed-off-by: Philip Oberfichtner <pro@denx.de>

--- arch/mips/mach-mtmips/mt7628/serial.c
+++ arch/mips/mach-mtmips/mt7628/serial.c
@@ -11,7 +11,7 @@
 void mtmips_spl_serial_init(void)
 {
 #ifdef CONFIG_SPL_SERIAL
-	void __iomem *base = ioremap_nocache(SYSCTL_BASE, SYSCTL_SIZE);
+	void __iomem __maybe_unused *base = ioremap_nocache(SYSCTL_BASE, SYSCTL_SIZE);
 
 #if CONFIG_CONS_INDEX == 1
 	clrbits_32(base + SYSCTL_GPIO_MODE1_REG, UART0_MODE_M);
